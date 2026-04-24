66bac42e8d977c14fd52a389da7d371662b5ac86
Hedyeh Ghesmati Kalourazi <hghesmati-kalourazi@myseneca.ca>
2026-04-24 10:19:16 -0400

    React version of psychologist website

---
+++ react-site/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+function Navbar() {
+  return (
+    <header>
+      <nav>
+
+        <div>
+          <span>Dr Arash Clinic</span>
+        </div>
+
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Contact</li>
+        </ul>
+
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
